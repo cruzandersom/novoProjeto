bc4dcc074fa6175fcab93b1c8822716d08ba0fd0
cruzandersom <cruz.andersom@gmail.com>
2022-08-21 22:23:28 -0300

    inicio_da_aplicacao

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Cesta de produtos</Text>
       <StatusBar style="auto" />
     </View>
   );
